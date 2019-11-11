a9d2c32eb530e4d59616462a6cde3cf51537783f
asmarsel <asmarsel@gmail.com>
2019-11-11 19:29:45 +0200

    Source Code Commit

---
+++ lesson2/src/main/java/edu/jcourse/lesson4/LogicalTableDemo.java
@@ -0,0 +1,8 @@
+package edu.jcourse.lesson4;
+//todo "homework"
+public class LogicalTableDemo {
+    public static void main(String[] args) {
+        boolean a, b;
+        System.out.println("A\t\tB\t\tAND");
+    }
+}
